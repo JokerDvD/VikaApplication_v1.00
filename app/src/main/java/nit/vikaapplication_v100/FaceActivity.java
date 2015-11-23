36e58279afc2e959f7aab711990d4063c66f8a0c
Slamaka Cool <shaqyrslam@gmail.com>
2015-11-23 22:49:28 +0300

    added QRCodeReader

--- app/src/main/java/nit/vikaapplication_v100/FaceActivity.java
+++ app/src/main/java/nit/vikaapplication_v100/FaceActivity.java
@@ -1,9 +1,8 @@
 package nit.vikaapplication_v100;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import nit.vikaapplication_v100.QRCodeActivity.QRCodeActivity;
 
 public class FaceActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +29,10 @@ public class FaceActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent=new Intent(FaceActivity.this, QRCodeActivity.class);
+                startActivity(intent);
             }
         });
 
