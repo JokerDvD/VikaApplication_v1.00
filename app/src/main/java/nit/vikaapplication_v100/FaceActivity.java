c6a00bdbd1572bd73337baf0f95fca55b4620f63
Slamaka Cool <shaqyrslam@gmail.com>
2015-11-24 13:26:59 +0600

    added landscape orientation

--- app/src/main/java/nit/vikaapplication_v100/FaceActivity.java
+++ app/src/main/java/nit/vikaapplication_v100/FaceActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,13 +18,14 @@ import nit.vikaapplication_v100.QRCodeActivity.QRCodeActivity;
 
 public class FaceActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+     View coordinatorLayoutView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        coordinatorLayoutView= findViewById(R.id.snackbarPosition);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,14 +67,35 @@ public class FaceActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (item.getItemId()){
+            case R.id.action_exit:
+
+
+                Snackbar
+                        .make(coordinatorLayoutView, "Test Action", Snackbar.LENGTH_LONG)
+                        .setAction("Test", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show();
+                            }
+                        })
+                        .show();
+                break;
+
+            case R.id.action_main:
+
+                break;
+
+            case R.id.action_camera:
+
+                break;
+
+            case R.id.action_settings:
+
+                break;
         }
 
         return super.onOptionsItemSelected(item);
